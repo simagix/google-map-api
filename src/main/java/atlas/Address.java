c97ead5eddcab7f202845f28dc039b875c3c6448
Kuei-chun Chen <simagix@gmail.com>
2017-02-08 23:59:02 -0500

    use spring cache
    Signed-off-by: Kuei-chun Chen <simagix@gmail.com>

--- src/main/java/atlas/Address.java
+++ src/main/java/atlas/Address.java
@@ -1,16 +1,26 @@
 package atlas;
 
+import java.io.IOException;
 import java.util.Date;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Address {
 	private String latLong;
 	private String formattedAddress;
 	private Date eventTime;
 
-	public Address(String latLong, String address) {
+	public Address(String latLong, String key) throws ClientProtocolException, IOException, InvalidInputException {
 		this.latLong = latLong;
-		this.formattedAddress = address;
 		this.eventTime = new Date();
+		getFromGoogleMap(latLong, key);
 	}
 
 	/**
@@ -35,5 +45,25 @@ public class Address {
 	 */
 	public Date getEventTime() {
 		return eventTime;
+	}
+
+	private void getFromGoogleMap(String latLong, String key)
+			throws InvalidInputException, ClientProtocolException, IOException {
+		String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latLong + "&key=" + key;
+		System.out.println("getFromGoogleMap " + url);
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(url);
+		request.addHeader("accept", "application/json");
+		HttpResponse response = client.execute(request);
+		String body = IOUtils.toString(response.getEntity().getContent(), "UTF-8");
+		JSONObject json = new JSONObject(body);
+		JSONArray array = json.getJSONArray("results");
+		for (int i = 0; i < array.length();) {
+			JSONObject object = array.getJSONObject(i);
+			this.formattedAddress = object.getString("formatted_address");
+			return;
+		}
+
+		throw new InvalidInputException();
 	}
 }
--- src/main/java/atlas/Controller.java
+++ src/main/java/atlas/Controller.java
@@ -1,25 +1,33 @@
 package atlas;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Component
 public class Controller {
+	private AddressRepo repo;
+    public Controller(AddressRepo repo) {
+        this.repo = repo;
+    }
+    
 	@RequestMapping("/geocode")
 	@ResponseBody
 	public ResponseEntity<String> geocode(@RequestParam(value = "latlng") String latLong, @RequestParam(value = "key") String apiKey,
             HttpServletRequest request, HttpServletResponse response) {
 		try {
-			return new ResponseEntity<String>(AddressRepo.getInstance().getAddress(latLong, apiKey).getFormattedAddress(), HttpStatus.OK);
+			return new ResponseEntity<String>(repo.getAddress(latLong, apiKey).getFormattedAddress(), HttpStatus.OK);
 		} catch (IOException e) {
 			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
 		} catch (InvalidInputException e) {
@@ -29,7 +37,7 @@ public class Controller {
 	
 	@RequestMapping("/geocode/history")
 	@ResponseBody
-	public ResponseEntity<Address[]> geocode(HttpServletRequest request, HttpServletResponse response) {
-		return new ResponseEntity<Address[]>(AddressRepo.getInstance().getHistory(), HttpStatus.OK);
+	public ResponseEntity<List<Address>> geocode(HttpServletRequest request, HttpServletResponse response) {
+		return new ResponseEntity<List<Address>>(repo.getCache(10), HttpStatus.OK);
 	}
 }
